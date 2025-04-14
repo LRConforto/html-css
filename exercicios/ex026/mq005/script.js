4503e84322942139470c25b00107685995b49a1d
Lucas Rodrigues <lrconforto@hotmail.com>
2025-04-14 14:06:25 -0300

    att Projeto media query mq005

--- exercicios/ex026/mq005/script.js
+++ exercicios/ex026/mq005/script.js
@@ -4,5 +4,42 @@ function clickMenu() {
     }
     else { 
         itens.style.display ='block'
+    }   
+}
+
+/* window.addEventListener('resize', () => {
+    if (window.innerWidth >= 768) {
+      itens.style.display = 'block'; // força visível no desktop
+    } else {
+      itens.style.display = 'none'; // esconde em telas menores
+    }
+  }); 
+  
+  Função anonima entregue pelo ChatGPT
+
+  */
+
+
+/* function ajustarMenu() { 
+    if(window.innerWidth >= 768){
+        itens.style.display = 'block'
+    } else { 
+        itens.style.display = 'none'
     }
 }
+
+window.addEventListener('resize', ajustarMenu) 
+
+função nomeada que o chat me mostrou.
+
+*/
+
+function mudouTamanho(){ 
+    if (window.innerWidth >= 768){
+        itens.style.display = 'block'
+    } else { 
+        itens.style.display = 'none'
+    }
+}
+
+/* Função escrita pelo guanabara no curso, praticamente mesma coisa da do chat porém ele chama a função pelo body em vez de adicionar o window eventlistener  */
